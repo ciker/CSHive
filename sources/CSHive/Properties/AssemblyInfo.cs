0297aad4feff6d9967278101134ac5573ee0503c
atwind <atwind@cszi.com>
2016-08-05 14:30:26 +0800

    CI only

--- sources/CSHive/Properties/AssemblyInfo.cs
+++ sources/CSHive/Properties/AssemblyInfo.cs
@@ -35,11 +35,15 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.2.0.22")] //已取消CI脚本的版本号替换功能，该版本号将发布至NuGet上。Note:版本号不变化时将不会发布至Nuget上
+[assembly: AssemblyVersion("3.2.1.0")] //已取消CI脚本的版本号替换功能，该版本号将发布至NuGet上。Note:版本号不变化时将不会发布至Nuget上
 //[assembly: AssemblyFileVersion("4.0.0.0")]
 
 
 /*
+
+v3.2.1.0
+------------------
+. 纯版本号变更
 
 v3.2.0.22
 ------------------
